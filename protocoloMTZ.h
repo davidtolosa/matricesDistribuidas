2b47660daa9b8e97d702e36fb7ba7f9a8952106f
Coronel, Renzo <renzo.h.coronel@gmail.com>
2016-01-25 10:25:33 -0300

    add protocolo.h

--- protocoloMTZ.h
+++ protocoloMTZ.h
@@ -2,3 +2,51 @@
 ARCHIVO DE DEFINICION DE PROTOCOLO.
 En este archivo se define el protocolo de alto nivel para comunicar nuestra app.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+//CODIGOS DE LOS MENSAJES
+//....................................................
+//Codigo del tipo 40X - CONFIRMACION ACK
+#define ACK 401
+//....................................................
+//codigos del tipo 10X - SOLICITUDES DE CONEXION.
+#define SOLICITUD_CLIENTE 101
+#define SOLICITUD_WORKER 102
+
+
+//DECLARACION DE LA ESTRUCTURA DEL MENSAJE
+#define HEADER_LENGHT sizeof(headerMTZ);
+
+//.....................................................
+typedef struct header
+{
+  int codigo;
+  uint32_t lenght; //corresponde a la longitud total del mensaje
+
+} headerMTZ;
+
+typedef struct body
+{
+  char *mensage;
+} bodyMTZ;
+
+typedef struct protocolo
+{
+    headerMTZ header;
+    bodyMTZ body;
+} protocoloMTZ;
+
+//DEFINICIONES DE LAS FUNCIONES PARA LOS MENSAJES
+//.....................................................
+/*
+
+*/
+int leerBytes(int sd, void *buffer, int len);
+/*
+
+*/
+
+char leer_mensaje(int sd, char ** PS_mensaje)
