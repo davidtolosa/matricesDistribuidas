972dabe78305e27465ba1aa2c5ba99283faa69e3
David Tolosa <davidtolosa5@gmail.com>
2016-02-01 18:19:14 -0300

    Fix ondeletecascade

--- DBManager.c
+++ DBManager.c
@@ -13,8 +13,10 @@ Retorna: Puntero a la conexion que se creo.
 sqlite3* db_openDB(int flag){
 
 	sqlite3 *handle;
+
 	int retval = sqlite3_open_v2("../dbMTZ.sqlite3",&handle,flag,0);
 
+	sqlite3_exec(handle, "PRAGMA foreign_keys = ON;", 0, 0, 0);
 
 	if(retval!=SQLITE_OK)
 	{
